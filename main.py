3591034360b073ed7784717365068e8e3c623c6a
Adam Kareen <me@akareen.com>
2023-10-02 13:53:48 +1100

    Added stats for all players

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from urllib.parse import urljoin
 
 base_url = "https://afltables.com/afl/stats/2023.html"
 
-
 def get_soup(url):
     try:
         headers = {"User-Agent": "Mozilla/5.0"}
@@ -48,8 +47,8 @@ def scrape_player_page(player_url, team_directory):
                 writer.writerow(data)
 
 
-def main():
-    soup = get_soup(base_url)
+def scrape_team_page_yearly(teams_url, directory):
+    soup = get_soup(teams_url)
     if not soup:
         return
 
@@ -59,7 +58,7 @@ def main():
             continue
 
         team_name = team_link.text.strip()
-        team_directory = os.path.join('data/game_stats', team_name)
+        team_directory = os.path.join(directory, team_name)
         os.makedirs(team_directory, exist_ok=True)
 
         for row in table.find_all('tr'):
@@ -71,5 +70,46 @@ def main():
                     scrape_player_page(player_url, team_directory)
 
 
+def scrape_site(start_url, directory):
+    soup = get_soup(start_url)
+    if not soup:
+        return
+    
+    # Find all team links on the main page
+    for a in soup.find_all('a', href=lambda x: x and 'teams/' in x):
+        intermediate_team_url = urljoin('https://afltables.com/afl/', a['href'])
+        
+        # Follow the intermediate team link
+        intermediate_team_soup = get_soup(intermediate_team_url)
+        if not intermediate_team_soup:
+            continue
+        
+        # Find the first link with 'stats/teams'
+        stats_link = intermediate_team_soup.find('a', href=lambda x: x and 'stats/teams' in x)
+        if not stats_link:
+            continue
+        
+        print(stats_link['href'])
+        # Construct the final team stats URL explicitly
+        final_team_stats_url = 'https://afltables.com/afl/' + stats_link['href'].replace('../', '')
+        print(final_team_stats_url)
+        final_team_stats_soup = get_soup(final_team_stats_url)
+        if not final_team_stats_soup:
+            continue
+        
+        team_name = os.path.basename(final_team_stats_url).replace('.html', '')
+        team_directory = os.path.join(directory, team_name)
+        os.makedirs(team_directory, exist_ok=True)
+
+        # Follow the final team stats URL and scrape every href starting with '../players'
+        for player_link in final_team_stats_soup.find_all('a', href=lambda x: x and x.startswith('../players')):
+            player_url = urljoin(final_team_stats_url, player_link['href'])
+            scrape_player_page(player_url, team_directory)
+
+
 if __name__ == "__main__":
-    main()
+    # Usage
+    start_url = 'https://afltables.com/afl/afl_index.html'
+    directory = 'data/team_stats_players_all_time'
+    scrape_site(start_url, directory)
+
